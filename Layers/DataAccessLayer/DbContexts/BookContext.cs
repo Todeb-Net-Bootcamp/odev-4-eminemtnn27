45e5f1e20e79d5d161ec687c4db3209dcf14b7d1
Emine Metin <eminemetin9@gmail.com>
2022-07-22 22:52:10 +0300

    4.ödev eklendi

---
+++ Layers/DataAccessLayer/DbContexts/BookContext.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+using Microsoft.EntityFrameworkCore; 
+using Models.Entities;
+
+#nullable disable
+
+namespace DataAccessLayer.DbContexts
+{
+    public partial class BookContext : DbContext
+    {
+        public BookContext()
+        {
+        }
+
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+        //tüm tablolarınn bulunduğu alan
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<BookAuthorPublish> BookAuthorPublishes { get; set; }
+        public virtual DbSet<Publisher> Publishers { get; set; }
+        public virtual DbSet<Reader> Readers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer("Server= EMINE-HOMEPC\\SQLEXPRESS;Database=emine;Trusted_Connection=True;"));
+        }
+
+    }
+}
---
+++ Layers/Models/Entities/Reader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Entities
+{
+    public class Reader
+    {
+        public int ReaderId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public ICollection<BookAuthorPublish> bookAuthorPublishes { get; set; }
+    }
+     
+}
